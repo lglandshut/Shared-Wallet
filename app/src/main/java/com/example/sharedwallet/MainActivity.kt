9e84615166429f9f150ec18fd7c9a1838a1b5a09
lukasgrimm <lglandshut@hotmail.de>
2024-11-06 12:09:45 +0100

    Logout logic and code cleanup

--- app/src/main/java/com/example/sharedwallet/MainActivity.kt
+++ app/src/main/java/com/example/sharedwallet/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.sharedwallet
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -11,6 +15,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import com.example.sharedwallet.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -22,41 +27,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var authManager: AuthManager
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+    private lateinit var navController: NavController
+    private lateinit var headerView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.appBarMain.toolbar)
-
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_groups, R.id.nav_activity, R.id.nav_friends
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
-        // Initialize Firebase Auth
-        auth = Firebase.auth
-        authManager = AuthManager(auth)
+        initViews()
+        initFirebase()
+        initListeners()
+        loadData()
     }
 
     public override fun onStart() {
         super.onStart()
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -68,5 +54,53 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun initViews() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.appBarMain.toolbar)
+        drawerLayout = binding.drawerLayout
+        navView = binding.navView
+        headerView = navView.getHeaderView(0)
+        navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_groups, R.id.nav_activity, R.id.nav_friends
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    private fun initListeners() {
+
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab).show()
+        }
+
+        headerView.findViewById<ImageView>(R.id.imageView_logout).setOnClickListener { _ ->
+            authManager.signOut()
+            //Back to LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish() // Schließt MainActivity
+        }
+    }
+
+    private fun initFirebase() {
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        authManager = AuthManager(auth)
+    }
+
+    private fun loadData() {
+        val textEmail = headerView.findViewById<TextView>(R.id.textEmail)
+        textEmail.text = auth.currentUser?.email ?: "Email not found!"
     }
 }
